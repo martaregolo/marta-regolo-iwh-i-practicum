f0f260a955e50843b2db38d1c92be3205ba405a2
Marta <martaregolo.collaboratore@supermoney.it>
2025-01-15 18:16:39 +0100

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -9,63 +9,64 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
+const hubspotUrl = 'https://api.hubapi.com/crm/v3/objects/2-39465700';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const propertiesUrl = 'https://api.hubapi.com/crm/v3/properties/2-39465700';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+  
+    try {
+      // Fetch all records of the custom object
+      const response = await axios.get(hubspotUrl, { headers });
+      const records = response.data.results;
+      const detailedRecords = await Promise.all(
+        records.map(async (record) => {
+          const recordUrl = `${hubspotUrl}/${record.id}?properties=name&properties=game&properties=role`;
+          const detailedResponse = await axios.get(recordUrl, { headers });
+          return detailedResponse.data;
+        })
+      );
+      console.log('Detailed records:', detailedRecords);
+      res.render('homepage', {
+        pageTitle: 'Homepage | Integrating With HubSpot I Practicum',
+        records: detailedRecords,
+        properties: ['name', 'game', 'role'],
+      });
+    } catch (error) {
+      console.error('Error fetching custom object records:', error.response?.data || error.message);
+      res.status(500).send('Error fetching custom object records.');
     }
-
-});
-*/
-
-
+  });
+  
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+  });
+  
+  app.post('/update-cobj', async (req, res) => {
+    const { name, game, role } = req.body;
+    const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json',
+    };
+  
+    const data = {
+      properties: {
+        name: name,
+        game: game,
+        role: role,
+      },
+    };
+  
+    try {
+      await axios.post(hubspotUrl, data, { headers });
+      res.redirect('/update-cobj');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error creating custom object record.');
+    }
+  });
+  
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
